7d76b63e777834756c19321a64dd4b95af33189c
Vlad Laptop <vladg13012007@gmail.com>
2025-03-25 18:44:11 +0300

    gt

--- Asm_Labs/Lab_3.asm
+++ Asm_Labs/Lab_3.asm
@@ -4,39 +4,37 @@ masm
 .stack 100h
 ;f = (12 - 96385) * x / (y - 3698)
 .data
-    x         db ?
-    y         db ?
-    result    dd ?
-    strokaX   db 'Input x: ',  '$'
-    strokaY   db 0Dh, 0Ah, 'Input y: ',  '$'
-    strokaRes db 0Dh, 0Ah, 'Result: $'
+    x db ?
+    y db ?       
+    result dd ?  
 .code
-    start:
-          mov  ax, @data
-          mov  ds, ax
+start:
+    mov ax, @data
+    mov ds, ax
+
+    mov ax, 12
+    cbw
+    sub eax, 96385
+
+    mov ebx, eax
+
+    mov al, x
+    cbw
+
+    imul ebx
+
+;edx
+    mov al, y
+    cbw 
+
+    sub eax, 3698
+    xchg eax, ebx
+    idiv ebx
 
 
-          mov  ax, 12
-          cbw
-          sub  eax, 96385
+    
 
-          mov  ebx, eax
-
-          mov  al, x
-          cbw
-
-          imul ebx, eax
-
-
-          mov  al, y
-          cbw
-
-          sub  eax, 3698
-          xchg eax, ebx
-          idiv ebx
-
-
-          mov  ax, 4C00h
-          int  21h
+    mov ax, 4C00h
+    int 21h
 
 end start
