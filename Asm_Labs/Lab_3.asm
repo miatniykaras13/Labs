5579c809c5b11bc9524e64b5e9f7e9657ec7f967
miatniykaras13 <vladosggg007@gmail.com>
2025-03-25 19:41:14 +0300

    лаба 3 сделана

--- Asm_Labs/Lab_3.asm
+++ Asm_Labs/Lab_3.asm
@@ -1,42 +1,45 @@
-masm
-.model small
+;(12 - 96385) * x / (y - 3698)
+masm    
+model small
 .386
 .stack 100h
-;f = (12 - 96385) * x / (y - 3698)
 .data
-    x         db ?
-    y         db ?
-    result    dd ?
-    strokaX   db 'Input x: ',  '$'
-    strokaY   db 0Dh, 0Ah, 'Input y: ',  '$'
-    strokaRes db 0Dh, 0Ah, 'Result: $'
+    x      db 1
+    y      db 0
+    z      db 0
+    result dd ?
 .code
     start:
-          mov  ax, @data
-          mov  ds, ax
+          mov   ax, @data
+          mov   ds, ax
 
+    ; 96385 = 0x17881
+          mov   ebx, 17881h
+          neg   ebx
 
-          mov  ax, 12
+          add   ebx, 0Ch                 ; 12 - 96385
+
+          mov   al, x
           cbw
-          sub  eax, 96385
+          imul  ebx                      ; (12 - 96385) * x
 
-          mov  ebx, eax
+          mov   bl, y
+          movsx ebx, bl
+          sub   ebx, 0E72h               ; y - 3698
 
-          mov  al, x
-          cbw
+          neg   ebx
+          neg   eax
+          mov   edx, 0
+          idiv  ebx
 
-          imul ebx, eax
+          mov   word ptr [result], ax
 
-
-          mov  al, y
-          cbw
-
-          sub  eax, 3698
-          xchg eax, ebx
-          idiv ebx
+    ; перемещение результата по адресу 1000h
+          mov   [ds:1000h], bx
+          mov   [ds:1002h], dx
 
 
-          mov  ax, 4C00h
-          int  21h
+          mov   ax, 4c00h
+          int   21h
 
 end start
